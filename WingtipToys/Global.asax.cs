a90a121ec22be0503a32980c3d6514e9ca10c036
ESSARRAJ <essarraj.fouad@gmail.com>
2018-02-06 22:11:42 +0100

    Create the Data Access Layer

--- WingtipToys/Global.asax.cs
+++ WingtipToys/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -16,6 +18,17 @@ namespace WingtipToys
             // Code qui s’exécute au démarrage de l’application
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize the product database.
+            Database.SetInitializer(new ProductDatabaseInitializer());
+
+            // Forces initialization of database on model changes.
+            using (var context = new ProductContext())
+            {
+                context.Database.CreateIfNotExists();
+            }
+
+
         }
     }
 }
---
+++ WingtipToys/Models/ProductContext.cs
@@ -0,0 +1,21 @@
+﻿namespace WingtipToys.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ProductContext : DbContext
+    {
+
+        public ProductContext()
+            : base("name=ProductContext")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+
+}
